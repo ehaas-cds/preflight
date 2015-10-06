45cdd8c4be7274557bea2b57111cc08888efb45a
HRISHIKESH S. KUMAR <hrishikesh.kumar@in.ibm.com>
2015-10-06 12:45:56 +0530

    Moved RHEL test to tests

--- ehaas_compute_medium.bash
+++ ehaas_compute_medium.bash
@@ -9,24 +9,8 @@ else
     rm util.bash
 fi
 
-# check for sudo access
-sudo -n true
-has_sudo_access=$? # 0 -> has access 1 -> no access
-
 # check if RHEL is registered
-if [ $has_sudo_access -eq 0 ]; then
-  subscription_status=$(subscription-manager list | grep Status: | awk 'NR==1{print $2}' | tr -d ' ')
-else
-  subscription_status="No Sudo Access"
-fi
-
-if [ "$subscription_status" = "No Sudo Access" ]; then
-  output_fail "Current user does not have Sudo Access to check RHEL Subscription"
-elif [ "$subscription_status" = "Subscribed" ]; then
-  output_ok "RHEL is registered with Red Hat Subscription Management"
-else
-  output_fail "RHEL is not registered with Red Hat Subscription Management"
-fi
+run_test "RHEL_subscription"
 
 # test CPU total (cores)
 run_test "cpu" 16
