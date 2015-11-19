569b27addf486a87ffee02942dff740b69815ca1
HRISHIKESH S. KUMAR <hrishikesh.kumar@in.ibm.com>
2015-11-19 14:34:31 +0530

    executing scripts from github broken - fix

--- ehaas_compute_small.bash
+++ ehaas_compute_small.bash
@@ -19,4 +19,8 @@ export NUM_DATA_DISKS
 PF_BI_PARTITION_DETAILS="/var 950 /boot 1 /opt 950 /home 950 /tmp 1450 /bigsqlhome 950 / 500"
 export PF_BI_PARTITION_DETAILS
 
-bash ./ehaas_template.bash
+if [ -f "$(dirname $0)/ehaas_template.bash" ]; then
+    bash ./ehaas_template.bash
+else
+    curl -s https://raw.githubusercontent.com/ehaas-cds/preflight/master/ehaas_template.bash | bash
+fi
